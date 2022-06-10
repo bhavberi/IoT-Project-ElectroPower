d18996906e8da7bfe7f3fa29301efa0a7a084fc1
Bhav Beri <beribhav@gmail.com>
2022-06-10 23:44:07 +0530

    Added bit of Formatting & some of the imports

--- Project_Code/Project_Code.ino
+++ Project_Code/Project_Code.ino
@@ -1,7 +1,21 @@
-// Libraries Needed to be installed - OneWire by Paul Stoffregen, DallasTemperature by Miles Burton
+/*
+=> Additional Board manager - https://dl.espressif.com/dl/package_esp32_index.json
+  Libraries Needed to be installed - 
+ *  OneWire by Paul Stoffregen
+ *  DallasTemperature by Miles Burton
+ *  ThingSpeak by MathWorks
+ *  PubSubClient by Nick O'Leary
+ *  WiFi (Built-in)
+*/
 
 #include <DallasTemperature.h>
 #include <OneWire.h>
+#include <ThingSpeak.h>
+#include <PubSubClient.h>
+#include <WiFiClient.h>
+#include <WiFi.h>
+
+// WATER TEMPERATURE SENSOR DETAILS
 
 #define BUZZER_PIN 18
 #define WATER_TEMP_PIN 15
@@ -14,6 +28,9 @@ OneWire oneWire(WATER_TEMP_PIN);
 DallasTemperature waterTempObj(&oneWire);
 
 float tempC = 0;
+
+// ----------------------------------
+// WATER LEVEL SENSOR DETAILS
 
 #define LVL_POWER_PIN 16
 #define WATER_SENSOR_PIN 33
@@ -30,6 +47,24 @@ float tempC = 0;
 #define LEVEL5 1600
 
 int waterLevelReading = 0;
+
+// ----------------------------------
+// THINGSPEAK DETAILS
+
+const char *ssid = "";
+const char *password = "";
+
+const char* server = "mqtt3.thingspeak.com";
+char mqttUserName[] = "bhavberi";
+char mqttPass[] = "5UKHX0B9UOS4MRAZ";
+long channelID = 0; // PUT ID
+char writeAPI[] = "0"; // PUT WRITE API
+
+// Setting up WiFi Client object
+WiFiClient client;
+// Setting up Publisher Sub-Client Object
+PubSubClient mqttClient(server, 1883, client);
+
 
 void setup() {
   Serial.begin(115200);
@@ -51,11 +86,30 @@ void setup() {
   pinMode(LED5, OUTPUT);
   digitalWrite(LED5, LOW); 
 
+  // ---------------------------------
+  // Beginnng/Setting up Water Temp Object
   waterTempObj.begin();
+
+  // ---------------------------------
+  //Setting up Wifi Connection
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // Setting up MQTT Broker details for ThingSpeak
+  mqttClient.setServer(server, 1883);
 }
 
 void loop() {
   // Reading value from Water Level Sensor using waterTempObj object
+  
   waterTempObj.requestTemperatures(); 
   tempC = waterTempObj.getTempCByIndex(0);
   Serial.println(tempC);
@@ -64,7 +118,8 @@ void loop() {
   {
     digitalWrite(BUZZER_PIN, LOW);
   }
-  
+
+  // ------------------------------------
   // Reading Water Level Value
   digitalWrite(LVL_POWER_PIN, HIGH);
   delay(5);
@@ -99,7 +154,11 @@ void loop() {
   {
     digitalWrite(LED5, HIGH);
   }
-  
+
+  // -------------------------------
+  // ThingSpeak Publishing the Data
+
+  // To be Added
   
   delay(500);
 }
